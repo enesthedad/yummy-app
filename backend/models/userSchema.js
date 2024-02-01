2ea6a75c61e7e553c06dc9bd1a191ab6f406d409
enesthedad <robinthelevi@gmail.com>
2024-02-01 12:36:57 +0300

    add user model and static register method

---
+++ backend/models/userSchema.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+
+const Schema = mongoose.Schema();
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+    minLength: 3,
+    maxLength: 64,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+  },
+});
+
+UserSchema.statics.register = async function (
+  email,
+  userName,
+  password,
+  fullName,
+  gender
+) {
+  if (!email || !password || !userName || !fullName) {
+    throw Error(`All fields need to be filled!`);
+  }
+  if (!validator(email)) {
+    throw Error(`${email} is not a valid email address`);
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error(`Please choose a stronger password!`);
+  }
+  const usernameExists = await this.findOne({ userName });
+  const emailExists = await this.findOne({ email });
+
+  if (email) {
+    throw Error(`You have already an account`);
+  }
+  if (userName) {
+    throw Error(`Username already taken please choose a different username!`);
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({
+    email,
+    userName,
+    password: hash,
+    fullName,
+    gender,
+  });
+  return user;
+};
